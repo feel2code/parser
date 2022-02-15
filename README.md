69f97d0697e1f9395b9eb795848984fa98ceed01
Felix Nabiullin <89178705098@mail.ru>
2022-02-15 23:56:24 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Parsing top films from site about films
+App parsing info and return file for database.
---
+++ top_films.py
@@ -0,0 +1,24 @@
+import time
+
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+data = []
+for year in range(2017, 2023):
+    pages = requests.get(f'https://www.kinoafisha.info/rating/movies/{year}/')
+    soup = BeautifulSoup(pages.text, 'html.parser')
+    time.sleep(1)
+    all_films = soup.findAll(
+        'a',
+        class_='movieItem_title'
+    )
+    for film in all_films:
+        text = film.text
+        link = film.get('href')
+        data.append([text, link, year])
+
+header = ('film_name', 'link', 'year')
+df = pd.DataFrame(data, columns=header)
+df.to_csv('/Users/felixmac/Desktop/films.csv', sep=';', encoding='utf8')
+# six, pytz, python-dateutil, numpy, pandas
