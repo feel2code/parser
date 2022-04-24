2812032dfef6278f44ec1e5e6fe9ad718d7044e0
Felix <90648171+feel2code@users.noreply.github.com>
2022-04-24 23:22:07 +0400

    Update top_films.py

--- top_films.py
+++ top_films.py
@@ -4,11 +4,19 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
+
 data = []
 for year in range(2017, 2023):
-    pages = requests.get(f'https://www.kinoafisha.info/rating/movies/{year}/')
-    soup = BeautifulSoup(pages.text, 'html.parser')
+    pages = requests.get(
+        f'https://www.kinoafisha.info/rating/movies/{year}/'
+    )
+    soup = BeautifulSoup(
+        pages.text,
+        'html.parser'
+    )
+    
     time.sleep(1)
+    
     all_films = soup.findAll(
         'a',
         class_='movieItem_title'
@@ -18,6 +26,17 @@ for year in range(2017, 2023):
         link = film.get('href')
         data.append([text, link, year])
 
-header = ('film_name', 'link', 'year')
-df = pd.DataFrame(data, columns=header)
-df.to_csv('/Users/user/Desktop/films.csv', sep=';', encoding='utf8')
+header = (
+    'film_name',
+    'link',
+    'year'
+)
+data_file = pd.DataFrame(
+    data,
+    columns=header
+)
+data_file.to_csv(
+    '/Users/user/Desktop/films.csv',
+    sep=';',
+    encoding='utf8'
+)
